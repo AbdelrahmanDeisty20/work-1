8753a93836d7a8ff9ab42e984d42c0b0973dba9b
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-04-26 14:35:41 +0300

    feat: add login view template with validation and registration link

--- resources/views/work/login.blade.php
+++ resources/views/work/login.blade.php
@@ -16,14 +16,14 @@
                     <label for="email" class="form-label">Email</label>
                     <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="Enter your email" value="{{ old('email') }}" >
                     @error('email')
-                        <div class="invalid-feedback">{{ $error }}</div>
+                        <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="mb-3">
                     <label for="password" class="form-label">Password</label>
                     <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password" placeholder="Enter your password" >
                     @error('password')
-                        <div class="invalid-feedback">{{ $error }}</div>
+                        <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <button type="submit" class="btn btn-primary w-100">Login</button>
